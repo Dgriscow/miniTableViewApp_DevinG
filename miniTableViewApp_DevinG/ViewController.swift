b3534e752d1369e2128d330975b3536b9f2db599
Dgriscow <dgrischow@students.mchenry.edu>
2023-03-01 10:55:56 -0600

    Initial Commit

---
+++ miniTableViewApp_DevinG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniTableViewApp_DevinG
+//
+//  Created by Devin Grischow on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
